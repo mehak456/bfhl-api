b34bdbeff8404c145d7fb60a01c497541262dd8f
Mehak <mehakarora567890@gmail.com>
2026-02-10 11:39:32 +0530

    Fix port for Render

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ app.post("/bfhl", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
